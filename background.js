80427a4e4eb2ab8ffc403e4e8def6783defb395b
Himanshu <himanshu.chhabra@rapyuta-robotics.com>
2020-10-26 22:40:39 +0530

    feat: implement mvp business logic

--- background.js
+++ background.js
@@ -2,10 +2,12 @@ chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.set({ color: "#3aa757" }, function () {
     console.log("The color is green.");
   });
+
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([
       {
         conditions: [
+          // hide url content in page (modify page) if meet.google.com/* (except meet.google.com/_meet/*)
           new chrome.declarativeContent.PageStateMatcher({
             pageUrl: { hostEquals: "meet.google.com" },
           }),
